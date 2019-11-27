fbfdabbaca672aa2989ec905d2914428fbe24319
daley <daley@.localdomain>
2019-11-27 17:47:43 +0800

    优化提示

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -29,11 +30,11 @@ func server() {
 }
 
 func init() {
-	fmt.Println("请输入需要监听的端口（多个端口用;分开，默认监听80、88端口）:")
+	fmt.Println("请输入需要监听的端口（多个端口用,分开，默认监听80,88两个端口）:")
 	var portScan string
 	n, _ := fmt.Scanln(&portScan)
 	if n != 0 {
-		ports = strings.Split(portScan, ";")
+		ports = strings.Split(portScan, ",")
 		for i := range ports {
 			if ports[i][0] != ':' {
 				ports[i] = ":" + ports[i]
@@ -54,5 +55,37 @@ func init() {
 		}
 	} else {
 		path = "/"
+	}
+
+	netInterface, err := net.Interfaces()
+	if err == nil {
+		// fmt.Print("检测到本机IP地址：")
+		for k := range netInterface {
+			if (netInterface[k].Flags & net.FlagUp) != 0 {
+				addrs, err := netInterface[k].Addrs()
+				if err == nil {
+
+					for _, addr := range addrs {
+						if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+							if ipnet.IP.To4() != nil {
+								// fmt.Println(ipnet.IP.String())
+								printListner(ipnet.IP.String())
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func printListner(ip string) {
+	fmt.Println("尝试在同一局域网设备浏览器中使用下述地址访问服务：")
+	for _, p := range ports {
+		if p == ":80" {
+			fmt.Println("http://" + ip + path)
+			continue
+		}
+		fmt.Println("http://" + ip + p + path)
 	}
 }
